68d2a9901245f85b28b50acefbedccc337ec2749
Dennis <glittenberg.d@gmail.com>
2020-10-15 11:29:29 +0200

    Anpassungen für ältere Delphi-Versionen

--- ExceptionlessLib.pas
+++ ExceptionlessLib.pas
@@ -3,10 +3,18 @@ unit ExceptionlessLib;
 interface
 
 uses
-  System.SysUtils, System.Net.HttpClient, System.Classes, System.Net.URLClient,
-  System.JSON, System.Generics.Collections;
+  System.SysUtils, System.Classes, {$IFDEF VER240}Data.DBXJson{$ELSE}System.JSON{$ENDIF}, System.Generics.Collections,
+  HttpClientWrapperLib;
 
 type
+  //This class is from here: https://stackoverflow.com/a/11786659
+  TSvJsonString = class(TJSONString)
+  private
+    function EscapeValue(const AValue: string): string;
+  public
+    constructor Create(const AValue: string); overload;
+  end;
+
   TExceptionlessEventType = (etLog, etError, etFeature);
 
   TExceptionlessUser = class(TObject)
@@ -34,15 +42,26 @@ type
     function ToJson: TJSONArray;
   end;
 
+  TExceptionlessData = TPair<String, String>;
+
+  TExceptionlessDataCollection = class(TList<TExceptionlessData>)
+  public
+    function ToJson: TJSONObject;
+  end;
+
   TExceptionlessEnvironment = class(TObject)
   private
     FMachineName: String;
     FProcessName: String;
+    FData: TExceptionlessDataCollection;
   public
+    constructor Create;
+    destructor Destroy; override;
     function ToJson: TJSONObject;
 
     property ProcessName: String read FProcessName write FProcessName;
     property MachineName: String read FMachineName write FMachineName;
+    property Data: TExceptionlessDataCollection read FData;
   end;
 
   TExceptionlessEvent = class(TObject)
@@ -55,6 +74,7 @@ type
     FVersion: String;
     FEnvironment: TExceptionlessEnvironment;
     FUserDescription: TExceptionlessUserDescription;
+    function getTypeAsString: String;
   public
     constructor Create;
     destructor Destroy; override;
@@ -85,10 +105,12 @@ type
 
   TExceptionless = class(TObject)
   private
+    FHttpWrapper: IHttpClientWrapper;
     FProjectId: String;
     FApiKey: String;
     FUserAgent: String;
   public
+    constructor Create(httpWrapper: IHttpClientWrapper);
     procedure Send(error: TExceptionlessSimpleError);
 
     property ProjectId: String read FProjectId write FProjectId;
@@ -103,39 +125,29 @@ uses
 
 { TExceptionless }
 
+constructor TExceptionless.Create(httpWrapper: IHttpClientWrapper);
+begin
+  FHttpWrapper := httpWrapper;
+end;
+
 procedure TExceptionless.Send(error: TExceptionlessSimpleError);
 const
   C_RequestUrl = 'https://collector.exceptionless.io/api/v2/projects/%s/events';
 var
-  client: THTTPClient;
   strStream: TStringStream;
-  header: TArray<TNameValuePair>;
+  header: TArray<THttpHeader>;
   url: string;
+  client: THttpClientWrapper;
 begin
   url := Format(C_RequestUrl, [ProjectId]);
 
   strStream := TStringStream.Create(error.ToString);
 
-  SetLength(header, 2);
-  header[0].Name := 'Authorization';
-  header[0].Value := Format('Bearer %s', [ApiKey]);
+  header := THttpHeader.SingleItem('Authorization', Format('Bearer %s', [ApiKey]));
+  THttpHeader.AddItem('userAgent', userAgent, header);
 
-  header[1].Name := 'userAgent';
-  header[1].Value := UserAgent;
-
-
-  TThread.CreateAnonymousThread(
-    procedure
-    begin
-      client := THTTPClient.Create;
-      client.ContentType := 'application/json';
-      try
-        client.Post(url, strStream, nil, header);
-      except
-      end;
-      client.Free;
-    end
-  ).Start;
+  client := THttpClientWrapper.Create(FHttpWrapper);
+  client.AsyncPost(url, 'application/json', strStream, header);
 end;
 
 { TExceptionlessSimpleError }
@@ -146,9 +158,9 @@ var
 begin
   Result := inherited;
   errorObj := TJSONObject.Create;
-  errorObj.AddPair('message', FMessage);
-  errorObj.AddPair('type', FExceptionType);
-  errorObj.AddPair('stack_trace', FStackTrace);
+  errorObj.AddPair('message', TSvJsonString.Create(FMessage));
+  errorObj.AddPair('type', TSvJsonString.Create(FExceptionType));
+  errorObj.AddPair('stack_trace', TSvJsonString.Create(FStackTrace));
 
   Result.AddPair('@simple_error', errorObj);
 end;
@@ -158,7 +170,7 @@ var
   jsonObj: TJSONObject;
 begin
   jsonObj := ToJson;
-  Result := jsonObj.ToJSON;
+  Result := jsonObj.ToString;
   jsonObj.Free;
 end;
 
@@ -167,8 +179,8 @@ end;
 function TExceptionlessUser.ToJson: TJSONObject;
 begin
   result := TJSONObject.Create;
-  result.AddPair('identity', FIdentity);
-  result.AddPair('name', FName);
+  result.AddPair('identity', TSvJsonString.Create(FIdentity));
+  result.AddPair('name', TSvJsonString.Create(FName));
 end;
 
 { TExceptionlessEvent }
@@ -190,13 +202,22 @@ begin
   inherited;
 end;
 
+function TExceptionlessEvent.getTypeAsString: String;
+begin
+  case FType of
+    etLog: Result := 'log';
+    etError: Result := 'error';
+    etFeature: Result := 'use';
+  end;
+end;
+
 function TExceptionlessEvent.ToJson: TJsonObject;
 begin
   Result := TJSONObject.Create;
-  Result.AddPair('type', 'error');
+  Result.AddPair('type', getTypeAsString);
   Result.AddPair('date', DateTimeToXMLTime(TimeStamp));
-  Result.AddPair('source', FSource);
-  Result.AddPair('@version', FVersion);
+  Result.AddPair('source', TSvJsonString.Create(FSource));
+  Result.AddPair('@version', TSvJsonString.Create(FVersion));
   Result.AddPair('tags', FTags.ToJson);
   Result.AddPair('@user', FUser.ToJson);
   Result.AddPair('@user_description', FUserDescription.ToJson);
@@ -218,11 +239,23 @@ end;
 
 { TExceptionlessEnvironment }
 
+constructor TExceptionlessEnvironment.Create;
+begin
+  FData := TExceptionlessDataCollection.Create;
+end;
+
+destructor TExceptionlessEnvironment.Destroy;
+begin
+  FData.Free;
+  inherited;
+end;
+
 function TExceptionlessEnvironment.ToJson: TJSONObject;
 begin
   Result := TJSONObject.Create;
-  Result.AddPair('ProcessName', ProcessName);
-  Result.AddPair('MachineName', MachineName);
+  Result.AddPair('ProcessName', TSvJsonString.Create(ProcessName));
+  Result.AddPair('MachineName', TSvJsonString.Create(MachineName));
+  Result.AddPair('Data', FData.ToJson);
 end;
 
 { TExceptionlessUserDescription }
@@ -230,8 +263,99 @@ end;
 function TExceptionlessUserDescription.ToJson: TJSONObject;
 begin
   Result := TJSONObject.Create;
-  Result.AddPair('Email_Address', EmailAddress);
-  Result.AddPair('Description', Description);
+  Result.AddPair('Email_Address', TSvJsonString.Create(EmailAddress));
+  Result.AddPair('Description', TSvJsonString.Create(Description));
+end;
+
+{ TExceptionlessDataCollection }
+
+function TExceptionlessDataCollection.ToJson: TJSONObject;
+var
+  data: TExceptionlessData;
+begin
+  Result := TJSONObject.Create;
+  for data in Self do
+  begin
+    Result.AddPair(data.Key, TSvJsonString.Create(data.Value));
+  end;
+end;
+
+{ TSvJsonString }
+
+
+constructor TSvJsonString.Create(const AValue: string);
+begin
+  inherited Create(EscapeValue(AValue));
+end;
+
+function TSvJsonString.EscapeValue(const AValue: string): string;
+
+ procedure AddChars(const AChars: string; var Dest: string; var AIndex: Integer); inline;
+  begin
+    System.Insert(AChars, Dest, AIndex);
+    System.Delete(Dest, AIndex + 2, 1);
+    Inc(AIndex, 2);
+  end;
+
+  procedure AddUnicodeChars(const AChars: string; var Dest: string; var AIndex: Integer); inline;
+  begin
+    System.Insert(AChars, Dest, AIndex);
+    System.Delete(Dest, AIndex + 6, 1);
+    Inc(AIndex, 6);
+  end;
+
+var
+  i, ix: Integer;
+  AChar: Char;
+begin
+  Result := AValue;
+  ix := 1;
+  for i := 1 to System.Length(AValue) do
+  begin
+    AChar :=  AValue[i];
+    case AChar of
+      '/', '\'(*, '"'*):
+      begin
+        System.Insert('\', Result, ix);
+        Inc(ix, 2);
+      end;
+      #8:  //backspace \b
+      begin
+        AddChars('\b', Result, ix);
+      end;
+      #9:
+      begin
+        AddChars('\t', Result, ix);
+      end;
+      #10:
+      begin
+        AddChars('\n', Result, ix);
+      end;
+      #12:
+      begin
+        AddChars('\f', Result, ix);
+      end;
+      #13:
+      begin
+        AddChars('\r', Result, ix);
+      end;
+      #0 .. #7, #11, #14 .. #31:
+      begin
+        AddUnicodeChars('\u' + IntToHex(Word(AChar), 4), Result, ix);
+      end
+      else
+      begin
+        if Word(AChar) > 127 then
+        begin
+          AddUnicodeChars('\u' + IntToHex(Word(AChar), 4), Result, ix);
+        end
+        else
+        begin
+          Inc(ix);
+        end;
+      end;
+    end;
+  end;
 end;
 
 end.
